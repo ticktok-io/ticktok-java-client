4ee224270054c67858ca9de2c4915f1beb012137
amaziagur <amazia@payayaglobal.com>
2018-05-11 23:07:24 +0300

    support deploy to jcenter

--- .circleci/destribute_to_jcenter.sh
+++ .circleci/destribute_to_jcenter.sh
@@ -7,7 +7,7 @@ distribute(){
     if [ "${CIRCLE_BRANCH}" == "master" ]; then
         version=$(git tag | sed -e 's/v//g')
         echo $version
-        mvn -s settings.xml deploy -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY -Dversion=$version
+#        mvn -s settings.xml deploy -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY -Dversion=$version
         echo "deploy"
     fi
 }
