a55c10a363aab726ccea4e65f8196e4d09e7721c
amaziagur <amazia@payayaglobal.com>
2018-05-11 23:22:35 +0300

    shift to git describe

--- .circleci/destribute_to_jcenter.sh
+++ .circleci/destribute_to_jcenter.sh
@@ -5,7 +5,7 @@ JQ="jq --raw-output --exit-status"
 
 distribute(){
     if [ "${CIRCLE_BRANCH}" == "master" ]; then
-        version=$(git tag | sed -e 's/v//g')
+        version=$(git describe | sed -e 's/v//g')
         echo "...." + $version
         mvn -s settings.xml deploy -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY -Dversion=$version
         echo "deploy"
