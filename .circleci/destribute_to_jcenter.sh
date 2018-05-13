e07b3408d83c827dfff4c1bcefd9edac56f6fe42
rockem <eli.segal@gmail.com>
2018-05-13 22:24:05 +0300

    Updated circleci script

--- .circleci/destribute_to_jcenter.sh
+++ .circleci/destribute_to_jcenter.sh
@@ -6,10 +6,8 @@ JQ="jq --raw-output --exit-status"
 
 distribute(){
     if [ "${CIRCLE_BRANCH}" == "master" ]; then
-        version=`git describe --tags --abbrev=0`
-        echo "...." + $version
-        mvn -s settings.xml deploy -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY -Dversion=$version
-        echo "deploy"
+        mvn -s settings.xml deploy -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY
+        echo "ticktok-java-client was deployed to jcenter"
     fi
 }
 
