56bbb68714d4d8a34139f26b62a45d9295269ed9
Eli Segal <eli.segal@gmail.com>
2019-04-16 07:36:21 +0300

    more deletions

--- src/main/java/io/ticktok/client/Ticktok.java
+++ src/main/java/io/ticktok/client/Ticktok.java
@@ -9,33 +9,13 @@ import io.ticktok.client.server.ClockRequest;
 public class Ticktok {
 
     private TicktokOptions options;
-    private String name;
-    private String schedule;
 
     public Ticktok(TicktokOptions options) {
         this.options = options;
     }
 
-    public Ticktok newClock(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public Ticktok on(String schedule) {
-        this.schedule = schedule;
-        return this;
-    }
-
-    public void invoke(Runnable runnable) {
-//        Clock clock = new RestClockCreator(this.options).create(ClockRequest.create(this.name, this.schedule));
-    }
-
     public static TicktokOptions options() {
         return new TicktokOptions();
-    }
-
-    public static ClockDetails clock() {
-        return new ClockDetails();
     }
 
     public void schedule(String name, String schedule, TickConsumer consumer) {
