86eec161d92d6a2c4a8f3c4ba8dfb03837f290fa
Eli Segal <eli.segal@gmail.com>
2019-04-20 14:11:42 +0300

    updated ci container

--- src/main/java/io/ticktok/client/server/ClockRequest.java
+++ src/main/java/io/ticktok/client/server/ClockRequest.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode
